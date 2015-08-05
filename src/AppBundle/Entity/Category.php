e4d49bdcb526ce2e00f2a75ddf5de3bb3efebc9b
Bogdan Carpusor <bogdan.carpusor@emag.ro>
2015-08-05 17:10:32 +0300

    added to string method

--- src/AppBundle/Entity/Category.php
+++ src/AppBundle/Entity/Category.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Category
@@ -25,6 +26,7 @@ class Category
      * @var string
      *
      * @ORM\Column(name="label", type="string", length=45, nullable=true)
+     * @Assert\NotBlank(message="The label cannot be empty")
      */
     private $label;
 
@@ -151,6 +153,21 @@ class Category
         return $this->product;
     }
 
+    public function __toString()
+    {
+        return $this->getLabel();
+    }
+
+    /**
+     * @Assert\True(message = "Parent category cannot be the same as child")
+     */
+    public function isNotSameAsParent()
+    {
+        if(!$this->getParentCategory()) {
+            return  true;
+        }
+        return $this->getId() !== $this->getParentCategory()->getId();
+    }
     const REPOSITORY = 'AppBundle:Category';
 }
 
