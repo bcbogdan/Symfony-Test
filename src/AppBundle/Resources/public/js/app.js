6c1195af694c11237b6412ccd5b13b87e1ae3b12
Bogdan Carpusor <bogdan.carpusor@emag.ro>
2015-08-05 17:08:39 +0300

    function for product acquisition history

--- src/AppBundle/Resources/public/js/app.js
+++ src/AppBundle/Resources/public/js/app.js
@@ -26,4 +26,23 @@ $(document).ready(function() {
         });
 
     })
+
+    var form = $('#product_history').parent();
+    var path = window.location.pathname;
+
+    form.on('submit', function(e){
+        e.preventDefault();
+        var id = path.split('/')[2];
+
+        $.ajax({
+            type: "POST",
+            dataType: "text",
+            url: Routing.generate('productacquisition_ajax_show', {id: id}),
+            success: function(response) {
+                $('#dialog').html(response);
+                $('#dialog').dialog();
+
+        }
+        })
+    })
 })
