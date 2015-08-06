4ee524d58b4f8fc6019c6756ce837303f1dd82b0
Bogdan Carpusor <bogdan.carpusor@emag.ro>
2015-08-06 17:50:52 +0300

    removed commented code

--- src/AppBundle/Controller/DashboardController.php
+++ src/AppBundle/Controller/DashboardController.php
@@ -9,18 +9,6 @@ class DashboardController extends Controller
 {
     public function indexAction()
     {
-//        $availableApiRoutes = [];
-//        foreach ($this->get('router')->getRouteCollection()->all() as $name => $route) {
-//            $route = $route->compile();
-//            if( strpos($name, "api_") !== 0 ){
-//                $emptyVars = [];
-//                foreach( $route->getVariables() as $v ){
-//                    $emptyVars[ $v ] = $v;
-//                }
-//                $url = $this->generateUrl( $name, $emptyVars );
-//                $availableApiRoutes[] = ["name" => $name, "url" => $url, "variables" => $route->getVariables()];
-//            }
-//      }
         $router = $this->get('router');
 
         $routes = array();
@@ -34,7 +22,6 @@ class DashboardController extends Controller
                 }
             }
         }
-      //  var_dump($routes); die('comment');
         return $this->render('AppBundle:Dashboard:index.html.twig', array(
             'routes' => $routeNames
         ));
